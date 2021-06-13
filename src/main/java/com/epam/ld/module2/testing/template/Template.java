0ab42d1b5faefa623240402a1e022b62f914807d
Katsiaryna_Kaloshych <koloshych@gmail.com>
2021-06-13 23:01:33 +0300

    generate message valid test

--- src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -5,4 +5,22 @@ package com.epam.ld.module2.testing.template;
  */
 public class Template {
 
+
+    public Template() {
+    }
+
+    public Template(String content) {
+        this.content = content;
+    }
+
+    private   String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -7,4 +7,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TemplateEngineTest {
 
+    private TemplateEngine templateEngine=new TemplateEngine();
+
+
+    @Test
+    void generateMessageValid(){
+        Template template=new Template("Dear #{name}, the purpose of my mail is #{subject}");
+        Client client=new Client("someEmail@gmail.com", "chess", "someName" );
+        String actual=templateEngine.generateMessage(template, client);
+        String expected="Dear someName, the purpose of my mail is chess";
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void generateMessageInValid(){
+
+    }
+
+    @Test
+    void generateMessageEmptyPlaceholder(){
+
+    }
+
+    @Test
+    void generateMessageFoundAllPlaceholders(){
+
+    }
+
+    @Test
+    void generateMessageFoundNotAllPlaceholders(){
+
+    }
+
+    @Test
+    void generateMessageFoundNothingPlaceholders(){
+
+    }
+
+
 }
